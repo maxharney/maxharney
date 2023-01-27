a0b273cb5d9341610bc0dea1ea28683284072b92
Max Harney <maxcharney@gmail.com>
2023-01-27 11:02:51 -0600

    Adding new favicon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -12,7 +12,7 @@ const config: GatsbyConfig = {
   plugins: ["gatsby-plugin-theme-ui", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      "icon": "src/images/icon.png"
+      "icon": "src/assets/images/awesome.png"
     }
   }, "gatsby-plugin-mdx", {
     resolve: 'gatsby-source-filesystem',
